57d8cd7aa3f6d162c41fb903a74a060fad96c407
Shawn Kothari <dkothari@email.arizona.edu>
2015-11-11 14:16:57 -0700

    got rid of most warnings except for unused variables

--- phase4/libuser.c
+++ phase4/libuser.c
@@ -21,322 +21,74 @@
 
 int Sleep(int seconds){
     systemArgs sysArg;
-    
     CHECKMODE;
-    sysArg.number = SYS_SLEEP; //No idea if this right right...
-    sysArg.arg1 = (void *) seconds;
+    sysArg.number = SYS_SLEEP;
+    sysArg.arg1 = (void *) ((long)seconds);
     USLOSS_Syscall(&sysArg);
     return (long) sysArg.arg4;
 }
 
 int DiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status){
     systemArgs sysArg;
-    
     CHECKMODE;
-    sysArg.number = SYS_DISKREAD; //again, no idea...
-    sysArg.arg1 = (void *) diskBuffer;
-    sysArg.arg2 = unit;
-    sysArg.arg3 = track; //might need to cast
-    sysArg.arg4 = first; //might need to cast
-    sysArg.arg5 = sectors; //might need to cast
+    sysArg.number = SYS_DISKREAD;
+    sysArg.arg1 = (void *) ((long)diskBuffer);
+    sysArg.arg2 = (void *) ((long)unit);
+    sysArg.arg3 = (void *) ((long)track);
+    sysArg.arg4 = (void *) ((long)first);
+    sysArg.arg5 = (void *) ((long)sectors);
     USLOSS_Syscall(&sysArg);
-    sysArg.arg4 = status; //might need to cast
-    return -1; //no idea to put here, look at specs
+    *status = (long) sysArg.arg1;
+    return (long) sysArg.arg4;
 }
 
 int DiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status){
     systemArgs sysArg;
-    
     CHECKMODE;
-    sysArg.number = SYS_DISKWRITE; //again, no idea...
-    sysArg.arg1 = (void *) diskBuffer;
-    sysArg.arg2 = unit;
-    sysArg.arg3 = track; //might need to cast
-    sysArg.arg4 = first; //might need to cast
-    sysArg.arg5 = sectors; //might need to cast
+    sysArg.number = SYS_DISKWRITE;
+    sysArg.arg1 = (void *) ((long)diskBuffer);
+    sysArg.arg2 = (void *) ((long)unit);
+    sysArg.arg3 = (void *) ((long)track);
+    sysArg.arg4 = (void *) ((long)first);
+    sysArg.arg5 = (void *) ((long)sectors);
     USLOSS_Syscall(&sysArg);
-    sysArg.arg4 = status; //might need to cast
-    return -1; //no idea to put here, look at specs
+    *status = (long) sysArg.arg1;
+    return (long) sysArg.arg4;
 }
 
 int DiskSize (int unit, int *sector, int *track, int *disk){
     systemArgs sysArg;
-    
     CHECKMODE;
-    sysArg.number = SYS_DISKSIZE; //again, no idea...
-    //sysArg.arg1 = (void *) diskBuffer;
-    sysArg.arg2 = unit;
-    sysArg.arg3 = track; //might need to cast
-    sysArg.arg4 = disk; //might need to cast
+    sysArg.number = SYS_DISKSIZE;
+    sysArg.arg1 = (void *) ((long)unit);
     USLOSS_Syscall(&sysArg);
-
-    return -1; //no idea to put here, look at specs
+    *disk = (long) sysArg.arg1;
+    *sector = (long) sysArg.arg2;
+    *track = (long) sysArg.arg3;
+    return (long) sysArg.arg4;
 }
 
 int TermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead){
     systemArgs sysArg;
-    
     CHECKMODE;
-    sysArg.number = SYS_TERMREAD; //again, no idea...
-    //sysArg.arg1 = (void *) diskBuffer;
-    sysArg.arg2 = buffer;
-    sysArg.arg3 = bufferSize; //might need to cast
-    sysArg.arg4 = unitID; //might need to cast
-    sysArg.arg5 = numCharsRead; //might need to cast
+    sysArg.number = SYS_TERMREAD;
+    sysArg.arg1 = (void *) ((long)*buffer);
+    sysArg.arg2 = (void *) ((long)bufferSize);
+    sysArg.arg3 = (void *) ((long)unitID);
     USLOSS_Syscall(&sysArg);
-
-    return -1; //no idea to put here, look at specs
+    *numCharsRead = (long) sysArg.arg2;
+    return (long) sysArg.arg4;
 }
+
 int TermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead){
     systemArgs sysArg;
-    
     CHECKMODE;
-    sysArg.number = SYS_TERMWRITE; //again, no idea...
-    //sysArg.arg1 = (void *) diskBuffer;
-    sysArg.arg2 = buffer;
-    sysArg.arg3 = bufferSize; //might need to cast
-    sysArg.arg4 = unitID; //might need to cast
-    sysArg.arg5 = numCharsRead; //might need to cast
+    sysArg.number = SYS_TERMWRITE;
+    sysArg.arg1 = (void *) ((long)*buffer);
+    sysArg.arg2 = (void *) ((long)bufferSize);
+    sysArg.arg3 = (void *) ((long)unitID);
     USLOSS_Syscall(&sysArg);
-
-    return -1; //no idea to put here, look at specs
+    *numCharsRead = (long) sysArg.arg2;
+    return (long) sysArg.arg4;
 }
-
-// /*
-//  *  Routine:  Spawn
-//  *
-//  *  Description: This is the call entry to fork a new user process.
-//  *
-//  *  Arguments:    char *name    -- new process's name
-//  *		  PFV func      -- pointer to the function to fork
-//  *		  void *arg	-- argument to function
-//  *                int stacksize -- amount of stack to be allocated
-//  *                int priority  -- priority of forked process
-//  *                int  *pid      -- pointer to output value
-//  *                (output value: process id of the forked process)
-//  *
-//  *  Return Value: 0 means success, -1 means error occurs
-//  */
-// int Spawn(char *name, int (*func)(char *), char *arg, int stack_size, 
-//     int priority, int *pid)   
-// {
-//     systemArgs sysArg;
-    
-//     CHECKMODE;
-//     sysArg.number = SYS_SPAWN;
-//     sysArg.arg1 = (void *) func;
-//     sysArg.arg2 = arg;
-//     sysArg.arg3 = (void *) ( (long) stack_size);
-//     sysArg.arg4 = (void *) ( (long) priority);
-//     sysArg.arg5 = name;
-//     USLOSS_Syscall(&sysArg);
-//     *pid = (long) sysArg.arg1;
-
-//     return (long) sysArg.arg4;
-// } /* end of Spawn */
-
-
-
-
-// /*
-//  *  Routine:  Wait
-//  *
-//  *  Description: This is the call entry to wait for a child completion
-//  *
-//  *  Arguments:    int *pid -- pointer to output value 1
-//  *                (output value 1: process id of the completing child)
-//  *                int *status -- pointer to output value 2
-//  *                (output value 2: status of the completing child)
-//  *
-//  *  Return Value: 0 means success, -1 means error occurs
-//  */
-// int Wait(int *pid, int *status)	
-// {
-//     systemArgs sysArg;
-    
-//     CHECKMODE;
-//     sysArg.number = SYS_WAIT;
-//     USLOSS_Syscall(&sysArg);
-//     *pid = (int) sysArg.arg1;
-//     *status = (int) sysArg.arg2;
-//     return (int) sysArg.arg4;
-    
-// } /* End of Wait */
-
-
-// /*
-//  *  Routine:  Terminate
-//  *
-//  *  Description: This is the call entry to terminate 
-//  *               the invoking process and its children
-//  *
-//  *  Arguments:   int status -- the commpletion status of the process
-//  *
-//  *  Return Value: 0 means success, -1 means error occurs
-//  */
-// void Terminate(int status)
-// {
-//     systemArgs sysArg;
-    
-//     CHECKMODE;
-//     sysArg.number = SYS_TERMINATE;
-//     sysArg.arg1 = (void *) ( (long) status);
-//     USLOSS_Syscall(&sysArg);
-//     return;
-    
-// } /* End of Terminate */
-
-
-// /*
-//  *  Routine:  SemCreate
-//  *
-//  *  Description: Create a semaphore.
-//  *		
-//  *  Arguments:    int value -- initial semaphore value
-//  *		  int *semaphore -- semaphore handle
-//  *                (output value: completion status)
-//  */
-// int SemCreate(int value, int *semaphore)
-// {
-//     systemArgs sysArg;
-
-//     CHECKMODE;
-//     sysArg.number = SYS_SEMCREATE;
-//     sysArg.arg1 = (void *) ( (long) value);
-//     USLOSS_Syscall(&sysArg);
-//     *semaphore = (int) sysArg.arg1;
-//     return (int) sysArg.arg4;
-// } /* end of SemCreate */
-
-
-// /*
-//  *  Routine:  SemP
-//  *
-//  *  Description: "P" a semaphore.
-//  *		
-//  *
-//  *  Arguments:    int semaphore -- semaphore handle
-//  *                (output value: completion status)
-//  *
-//  */
-// int SemP(int semaphore)
-// {
-//     systemArgs sysArg;
-
-//     CHECKMODE;
-//     sysArg.number = SYS_SEMP;
-//     sysArg.arg1 = (void *) ( (long) semaphore);
-//     USLOSS_Syscall(&sysArg);
-//     return (int) sysArg.arg4;
-// } /* end of SemP */
-
-
-// /*
-//  *  Routine:  SemV
-//  *
-//  *  Description: "V" a semaphore.
-//  *		
-//  *
-//  *  Arguments:    int semaphore -- semaphore handle
-//  *                (output value: completion status)
-//  *
-//  */
-// int SemV(int semaphore)
-// {
-//     systemArgs sysArg;
-
-//     CHECKMODE;
-//     sysArg.number = SYS_SEMV;
-//     sysArg.arg1 = (void *) ( (long) semaphore);
-//     USLOSS_Syscall(&sysArg);
-//     return (int) sysArg.arg4;
-// } /* end of SemV */
-
-
-// /*
-//  *  Routine:  SemFree
-//  *
-//  *  Description: Free a semaphore.
-//  *		
-//  *
-//  *  Arguments:    int semaphore -- semaphore handle
-//  *                (output value: completion status)
-//  *
-//  */
-// int SemFree(int semaphore)
-// {
-//     systemArgs sysArg;
-
-//     CHECKMODE;
-//     sysArg.number = SYS_SEMFREE;
-//     sysArg.arg1 = (void *) ( (long) semaphore);
-//     USLOSS_Syscall(&sysArg);
-//     return (int) sysArg.arg4;
-// } /* end of SemFree */
-
-
-// /*
-//  *  Routine:  GetTimeofDay
-//  *
-//  *  Description: This is the call entry point for getting the time of day.
-//  *
-//  *  Arguments:    int *tod  -- pointer to output value
-//  *                (output value: the time of day)
-//  *
-//  */
-// void GetTimeofDay(int *tod)                           
-// {
-//     systemArgs sysArg;
-    
-//     CHECKMODE;
-//     sysArg.number = SYS_GETTIMEOFDAY;
-//     USLOSS_Syscall(&sysArg);
-//     *tod = (int) sysArg.arg1;
-//     return;
-// } /* end of GetTimeofDay */
-
-
-// /*
-//  *  Routine:  CPUTime
-//  *
-//  *  Description: This is the call entry point for the process' CPU time.
-//  *		
-//  *
-//  *  Arguments:    int *cpu  -- pointer to output value
-//  *                (output value: the CPU time of the process)
-//  *
-//  */
-// void CPUTime(int *cpu)                           
-// {
-//     systemArgs sysArg;
-
-//     CHECKMODE;
-//     sysArg.number = SYS_CPUTIME;
-//     USLOSS_Syscall(&sysArg);
-//     *cpu = (int) sysArg.arg1;
-//     return;
-// } /* end of CPUTime */
-
-
-// /*
-//  *  Routine:  GetPID
-//  *
-//  *  Description: This is the call entry point for the process' PID.
-//  *		
-//  *
-//  *  Arguments:    int *pid  -- pointer to output value
-//  *                (output value: the PID)
-//  *
-//  */
-// void GetPID(int *pid)                           
-// {
-//     systemArgs sysArg;
-
-//     CHECKMODE;
-//     sysArg.number = SYS_GETPID;
-//     USLOSS_Syscall(&sysArg);
-//     *pid = (int) sysArg.arg1;
-//     return;
-// } /* end of GetPID */
-
 /* end libuser.c */
